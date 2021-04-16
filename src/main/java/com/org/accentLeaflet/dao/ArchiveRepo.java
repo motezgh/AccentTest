4fcb11091200e4a192e9f9696275439bd4bc9dfe
motezgh <motez.gharbi@hotmail.com>
2021-04-16 14:02:44 +0100

    CORRECTION! moving the repository to the DAO package

--- src/main/java/com/org/accentLeaflet/dao/ArchiveRepo.java
+++ src/main/java/com/org/accentLeaflet/dao/ArchiveRepo.java
@@ -1,4 +1,4 @@
-package com.org.accentLeaflet;
+package com.org.accentLeaflet.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
