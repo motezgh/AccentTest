41ac0914dd4ffa9e5205933da10f4c4a155c7f7b
motezgh <motez.gharbi@hotmail.com>
2021-04-16 11:36:26 +0100

    Creating spring boot app and adding MySQL Connector dependency to pom.xml and application.properties

---
+++ src/main/java/com/org/accentLeaflet/AccentLeafletApplication.java
@@ -0,0 +1,13 @@
+package com.org.accentLeaflet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentLeafletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentLeafletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/accentLeaflet/AccentLeafletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.accentLeaflet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentLeafletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
