57839ea1fb53efae863d359df58657cfa8f4a41f
motezgh <motez.gharbi@hotmail.com>
2021-04-16 12:06:56 +0100

    addig arch_1004901 class in entities package

---
+++ src/main/java/com/org/accentLeaflet/entities/Arch_1004901.java
@@ -0,0 +1,83 @@
+package com.org.accentLeaflet.entities;
+
+import java.time.LocalTime;
+
+import javax.persistence.Id;
+
+public class Arch_1004901 {
+
+	@Id
+	private LocalTime date ;
+	
+	@Id
+	private Long id_device ;
+	
+	private int speed ;
+	
+	private int fuel ;
+	
+	private int temp ;
+	
+	private int X ;
+	
+	private int Y ;
+	
+	private int Z ;
+	
+	private int ignition ;
+	
+	private int rpm ;
+	
+	private double fuel_rate ;
+	
+	private double tfu ;
+	
+	private double odo ;
+	
+	private int satInView ;
+	
+	private int signal ;
+	
+	private int heading ;
+	
+	private int charger ;
+	
+	private double latitude ;
+	
+	private double longitude ;
+	
+	private int stat ;
+	
+	private int tram_id ;
+	
+	private int validity ;
+	
+	private int temp_engine ;
+	
+	private float accum_odo ;
+	
+	private int do1 ;
+	
+	private int do2 ;
+	
+	private int do3 ;
+	
+	private int do4 ;
+	
+	private int di1 ;
+	
+	private int di2 ;
+	
+	private int di3 ;
+	
+	private int di4 ;
+	
+	private int an1 ;
+	
+	private int an2 ;
+	
+	private int an3 ;
+	
+	private int an4 ;
+	
+}
